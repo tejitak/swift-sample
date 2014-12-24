8a0f608de5670adfedec7fb764f33183518b0ba8
takuya.tejima <takuya.tejima@linecorp.com>
2014-12-24 17:15:32 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by TEJIMA TAKUYA on 2014/12/24.
+//  Copyright (c) 2014年 TEJIMA TAKUYA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
